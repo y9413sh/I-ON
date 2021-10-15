2969d585a69d55ec014ea49395fed09d630d8c5d
| YEE | <55368870+y9413sh@users.noreply.github.com>
2021-10-15 09:17:31 +0900

    Update day34

--- Vue/day34/App.vue
+++ Vue/day34/App.vue
@@ -1,87 +1,148 @@
 <template>
-  <div>
-    <form v-on:submit.prevent="submitForm">
-      <!-- <form v-on:submit="submitForm"> -->
-      <div>
-        <label for="username">ID: </label>
-        <input type="text" id="username" class="username-input" v-model="username" v-bind:class="{'error': isError}">
-      </div>
-      <div>
-        <label for="password">PW: </label>
-        <input type="password" id="password" v-model="password">
-      </div>
-      <button v-bind:disabled="!isUsernameValid" type="submit">로그인</button>
-    </form>
-    <!-- <p v-if="isError">올바르지 않은 ID 입니다.</p> -->
-    <p v-if="isUsernameValid">아이디가 이메일 형식이 맞습니다.</p>
-    <p v-if="isSuccess">로그인이 되었습니다.</p>
+  <div id="app">
 
-    <!-- <toast-popup v-bind:open="isSuccess"></toast-popup> -->
-    <ToastPopup v-bind:open="isSuccess" v-on:close="isSuccess = false"></ToastPopup>
+  <div class="black-bg" v-if="isModalOpen" >
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용</p>
+        <button @click="isModalOpen=false">닫기</button>
+      </div>
+    </div>
+
+    <div class="menu">
+      <!-- <a v-for="반복할 변수 in 숫자" :key="변수"> </a> -->
+      <!-- <a v-for="i in 3" :key="i">home </a> -->
+      <a v-for="menu in menus" :key="menu">{{menu}} </a>
+
+       <!-- <a v-for="(menu,i)in menus" :key="i">{{i}} </a>
+         <a v-for="(변수이름정의,인덱스번호)in 배열" :key="인덱스번호">{{인덱스번호}} </a> -->
+
+
+      <!-- <a>{{menus}}</a>
+      <a>{{menus[1]}}</a>
+      <a>{{menus[3]}}</a> -->
+      
+  </div>
+   <h1 class="h1con"> land world 왔니? </h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+
+  <div v-for="(p, i) in products" :key="i">
+  
+
+    <img src= "./assets/1.jpg">
+    <!-- <h4 :style="type"> {{products[i]}}원룸 </h4> 스타일 직접넣기 -->
+    <h4 @click="isModalOpen =true"> {{products[i]}}원룸 </h4>
+    <p> {{price}}만원</p>
+    
+    <button @click="increase(i)">허위매물신고</button>
+    <span>신고수:{{counts[i]}}</span>
+
+  </div>
+    
+   <!-- <div>
+      <img src= "./assets/2.jpg">
+    <h4> {{products[1]}}원룸 </h4>
+    <p> {{price2}}만원</p>
+    <button @click="increase()">허위매물신고</button>
+    <span>신고고고수:{{counts[i]}}</span>
+  </div>
+  <div>
+     <img src= "./assets/3.jpg">
+    <h4> {{products[2]}}원룸 </h4>
+    <p> 30만원</p>
+    <button @click="increase()">허위매물신고</button>
+    <span>신고고고수:{{counts[i]}}</span>
+  </div> -->
+ 
+
+
   </div>
 </template>
 
 <script>
-  // import ToastPopup from '@/components/ToastPopup.vue';
-  import ToastPopup from './components/ToastPopup.vue';
 
-  // 이메일에 대한 유효성 검사
-  function validateEmail(email) {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
 
-  export default {
-    components: {
-      ToastPopup
-    },
-    data() {
-      return {
-        username: '',
-        password: '',
-        isError: false,
-        isSuccess: false
-      }
-    },
-    computed: {
-      isUsernameValid() {
-        if (validateEmail(this.username)) {
-          this.isError = false;
-          return true;
-        } else {
-          this.isError = true;
-          return false;
-        }
-        // return validateEmail(this.username);
-      }
-    },
-    methods: {
-      submitForm() {
-        // submitForm(event) {
-        // event.preventDefault();
-        console.log('로그인');
-        // axios
-        //   .post()
-        //   .then()
-        //   .catch(error => {
-        //     this.isError = true;
-        //   });
-
-        this.isError = false;
-        this.initForm();
-      },
-      initForm() {
-        this.username = '';
-        this.password = '';
-        this.isSuccess = true;
-      }
+export default {
+  name: 'App',
+  data(){
+    return {
+      price: 70,
+      price2: 50,
+      type: 'color: red',
+      menus :['home', 'shop', 'products','about'],
+      products:['서대문구', '관악구','강서구'],
+      counts :[0,0,0],
+      isModalOpen: false,
     }
-  }
+  },
+  methods :{
+    increase(i) {
+      this.$set(this.counts,i,this.counts[i]+1);
+      
+       //반드시 앞에 this붙여야지 적용됨
+      // this.$set(obj, key, value)
+      // this.$set(arr, index, value)
+
+    }
+  },
+}
+  
 </script>
 
 <style>
-  .username-input.error {
-    border: 1px solid red;
-  }
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background:  rgb(0, 0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg{
+  width: 100%;
+  
+  background-color: whitesmoke;
+  border-radius: 8px;
+  padding: 20px;
+
+}
+/* .img{
+  width: 50px;
+  height: 50px;
+} */
+
+
+
+.menu{
+  background-color: darkgoldenrod;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: aliceblue;
+  padding: 10px;
+}
+
+.h1con {
+  color: blue;
+  font-family: 맑은 고딕;
+}
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
